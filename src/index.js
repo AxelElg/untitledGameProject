f0c8447ef745820bc43fa6ff3f32e24c704b4c31
Axel Elgenstierna <axelelgenstierna@Axels-MacBook-Pro-2.local>
2020-07-22 00:17:31 +0200

    refactored the code into different scenes and implemented the first enemy ai

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import Phaser from 'phaser';
-import dude from './assets/dude.png';
-import enemy from './assets/enemy.png';
-import ground from './assets/ground.png';
-import platform from './assets/platform.png';
+// import dude from './assets/dude.png';
+// import enemy from './assets/enemy.png';
+// import ground from './assets/ground.png';
+// import platform from './assets/platform.png';
+import scene2 from './Scenes/scene2';
 
-const config = {
+export const config = {
 	type: Phaser.AUTO,
 	width: 200,
 	height: 360,
@@ -15,158 +16,157 @@ const config = {
 			debug: false,
 		},
 	},
-	scene: {
-		preload: preload,
-		create: create,
-		update: update,
-	},
+	scene: scene2,
+	// {
+	// 	preload: preload,
+	// 	create: create,
+	// 	update: update,
+	// },
 	pixelArt: true,
 };
 
 const game = new Phaser.Game(config);
 
-function preload() {
-	this.load.image('ground', ground);
-	this.load.image('platform', platform);
-	this.load.spritesheet('dude', dude, {
-		frameWidth: 20,
-		frameHeight: 20,
-	});
-	this.load.spritesheet('enemy', enemy, {
-		frameWidth: 20,
-		frameHeight: 20,
-	});
-}
+// function preload() {
+// 	this.load.image('ground', ground);
+// 	this.load.image('platform', platform);
+// 	this.load.spritesheet('dude', dude, {
+// 		frameWidth: 20,
+// 		frameHeight: 20,
+// 	});
+// 	this.load.spritesheet('enemy', enemy, {
+// 		frameWidth: 20,
+// 		frameHeight: 20,
+// 	});
+// }
 
-let platforms;
-let player;
-let enemy1;
-let cursors;
+// let platforms;
+// let player;
+// let enemy1;
+// let cursors;
 
-function create() {
-	platforms = this.physics.add.staticGroup();
-	platforms.create(config.width / 2, config.height - 20, 'ground');
-	platforms.create(config.width / 4, (config.height - 20) / 2, 'platform');
-	platforms.create(
-		(config.width / 4) * 3,
-		(config.height - 20) / 4,
-		'platform'
-	);
-	platforms.create(
-		config.width / 2,
-		((config.height - 20) / 4) * 3,
-		'platform'
-	);
+// function create() {
+// 	platforms = this.physics.add.staticGroup();
+// 	platforms.create(config.width / 2, config.height - 20, 'ground');
+// 	platforms.create(config.width / 4, (config.height - 20) / 2, 'platform');
+// 	platforms.create(
+// 		(config.width / 4) * 3,
+// 		(config.height - 20) / 4,
+// 		'platform'
+// 	);
+// 	platforms.create(
+// 		config.width / 2,
+// 		((config.height - 20) / 4) * 3,
+// 		'platform'
+// 	);
 
-	player = this.physics.add.sprite(
-		config.width / 2,
-		config.height - 60,
-		'dude'
-	);
-	player.checkWorldBounds = true;
-	player.faceDir = 'right';
+// 	player = this.physics.add.sprite(
+// 		config.width / 2,
+// 		config.height - 60,
+// 		'dude'
+// 	);
+// 	player.checkWorldBounds = true;
+// 	player.faceDir = 'right';
 
-	enemy1 = this.physics.add.sprite(
-		config.width / 2,
-		(config.height / 3) * 2,
-		'enemy'
-	);
+// 	enemy1 = this.physics.add.sprite(
+// 		config.width / 2,
+// 		(config.height / 3) * 2,
+// 		'enemy'
+// 	);
 
-	this.anims.create({
-		key: 'right',
-		frames: this.anims.generateFrameNumbers('dude', { start: 7, end: 10 }),
-		frameRate: 10,
-		repeat: -1,
-	});
-	this.anims.create({
-		key: 'left',
-		frames: this.anims.generateFrameNumbers('dude', { start: 1, end: 4 }),
-		frameRate: 10,
-		repeat: -1,
-	});
-	this.anims.create({
-		key: 'floatLeft',
-		frames: [{ key: 'dude', frame: 0 }],
-		frameRate: 10,
-	});
-	this.anims.create({
-		key: 'floatRight',
-		frames: [{ key: 'dude', frame: 11 }],
-		frameRate: 10,
-	});
-	this.anims.create({
-		key: 'stillRight',
-		frames: [{ key: 'dude', frame: 6 }],
-		frameRate: 10,
-	});
-	this.anims.create({
-		key: 'stillLeft',
-		frames: [{ key: 'dude', frame: 5 }],
-		frameRate: 10,
-	});
-	player.anims.play('stillRight', true);
+// 	this.anims.create({
+// 		key: 'right',
+// 		frames: this.anims.generateFrameNumbers('dude', { start: 7, end: 10 }),
+// 		frameRate: 10,
+// 		repeat: -1,
+// 	});
+// 	this.anims.create({
+// 		key: 'left',
+// 		frames: this.anims.generateFrameNumbers('dude', { start: 1, end: 4 }),
+// 		frameRate: 10,
+// 		repeat: -1,
+// 	});
+// 	this.anims.create({
+// 		key: 'floatLeft',
+// 		frames: [{ key: 'dude', frame: 0 }],
+// 		frameRate: 10,
+// 	});
+// 	this.anims.create({
+// 		key: 'floatRight',
+// 		frames: [{ key: 'dude', frame: 11 }],
+// 		frameRate: 10,
+// 	});
+// 	this.anims.create({
+// 		key: 'stillRight',
+// 		frames: [{ key: 'dude', frame: 6 }],
+// 		frameRate: 10,
+// 	});
+// 	this.anims.create({
+// 		key: 'stillLeft',
+// 		frames: [{ key: 'dude', frame: 5 }],
+// 		frameRate: 10,
+// 	});
+// 	player.anims.play('stillRight', true);
 
-	this.anims.create({
-		key: 'enemyLeft',
-		frames: this.anims.generateFrameNumbers('enemy', { start: 0, end: 1 }),
-		frameRate: 3,
-		repeat: -1,
-	});
-	enemy1.anims.play('enemyLeft', true);
+// 	this.anims.create({
+// 		key: 'enemyLeft',
+// 		frames: this.anims.generateFrameNumbers('enemy', { start: 0, end: 1 }),
+// 		frameRate: 3,
+// 		repeat: -1,
+// 	});
+// 	enemy1.anims.play('enemyLeft', true);
 
-	this.physics.add.collider(player, platforms);
-}
+// 	this.physics.add.collider(player, platforms);
+// }
 
-function update() {
-	cursors = this.input.keyboard.createCursorKeys();
+// function update() {
+// 	cursors = this.input.keyboard.createCursorKeys();
 
-	if (cursors.left.isDown) {
-		player.setVelocityX(-60);
-		player.faceDir = 'left';
-		if (player.body.touching.down) {
-			player.anims.play('left', true);
-		} else {
-			player.anims.play('floatLeft', true);
-		}
-	} else if (cursors.right.isDown) {
-		player.setVelocityX(60);
-		player.faceDir = 'right';
-		if (player.body.touching.down) {
-			player.anims.play('right', true);
-		} else {
-			player.anims.play('floatRight', true);
-		}
-	} else {
-		player.setVelocityX(0);
-		if (player.faceDir === 'right') {
-			player.anims.play('stillRight', true);
-		} else {
-			player.anims.play('stillLeft', true);
-		}
-	}
-	if (!player.body.touching.down) {
-		if (player.faceDir == 'right') {
-			player.anims.play('floatRight', true);
-		} else {
-			player.anims.play('floatLeft', true);
-		}
-	}
+// 	if (cursors.left.isDown) {
+// 		player.setVelocityX(-60);
+// 		player.faceDir = 'left';
+// 		if (player.body.touching.down) {
+// 			player.anims.play('left', true);
+// 		} else {
+// 			player.anims.play('floatLeft', true);
+// 		}
+// 	} else if (cursors.right.isDown) {
+// 		player.setVelocityX(60);
+// 		player.faceDir = 'right';
+// 		if (player.body.touching.down) {
+// 			player.anims.play('right', true);
+// 		} else {
+// 			player.anims.play('floatRight', true);
+// 		}
+// 	} else {
+// 		player.setVelocityX(0);
+// 		if (player.faceDir === 'right') {
+// 			player.anims.play('stillRight', true);
+// 		} else {
+// 			player.anims.play('stillLeft', true);
+// 		}
+// 	}
+// 	if (!player.body.touching.down) {
+// 		if (player.faceDir == 'right') {
+// 			player.anims.play('floatRight', true);
+// 		} else {
+// 			player.anims.play('floatLeft', true);
+// 		}
+// 	}
 
-	if (cursors.up.isDown && player.body.velocity.y > -200) {
-		player.setVelocityY(player.body.velocity.y - 10);
-	}
+// 	if (cursors.up.isDown && player.body.velocity.y > -200) {
+// 		player.setVelocityY(player.body.velocity.y - 10);
+// 	}
 
-	if (player.x < -10) {
-		player.x = 210;
-	}
-	if (player.x > 210) {
-		player.x = -10;
-	}
-	if (player.y < 0) {
-		player.setVelocityY(200);
-	}
+// 	if (player.x < -10) {
+// 		player.x = 210;
+// 	}
+// 	if (player.x > 210) {
+// 		player.x = -10;
+// 	}
+// 	if (player.y < 0) {
+// 		player.setVelocityY(200);
+// 	}
 
-	enemy1.setVelocityY(-1);
-}
-
-console.log('hello world');
+// 	enemy1.setVelocityY(-1);
+// }
